fa580bb2d9e844ee0005372bd296059de734435f
Thiago Garcia <tgalves18@gmail.com>
2018-02-05 17:40:54 -0200

    Project basic structure

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Thiago Garcia on 05/02/18.
+//  Copyright © 2018 iGarcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    let emojiChoices = ["🎃", "👻", "🎃", "👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
+}
+
